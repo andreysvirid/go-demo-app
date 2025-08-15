60fe9cdae6c7d522f07c2af29b953d0321b62e50
Andrii.Svyrydenko <andrey_svirid@yahoo.com>
2025-08-16 01:09:06 +0200

    del patch add shell

---
+++ yaml/patch1.sh
@@ -0,0 +1,30 @@
+kubectl patch deployment ambassador -n demo2 --type='merge' -p '{
+  "spec": {
+    "template": {
+      "spec": {
+        "serviceAccountName": "ambassador",
+        "securityContext": {
+          "fsGroup": 1000
+        },
+        "containers": [
+          {
+            "name": "ambassador",
+            "image": "datawire/ambassador:2.0",
+            "command": ["/bin/sh","-c"],
+            "args": ["ulimit -n 65535 && exec ambassador"],
+            "securityContext": {
+              "runAsUser": 1000,
+              "runAsGroup": 1000
+            },
+            "resources": {
+              "limits": {
+                "cpu": "500m",
+                "memory": "512Mi"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}'
